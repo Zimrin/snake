db40fcbd1948a964b42c2b87222d1327d1af8d32
Simrin Siby <simrin2002@gmail.com>
2022-10-14 21:43:47 +0530

    subject reacts to input and detects bait

--- snake.rb
+++ snake.rb
@@ -1,54 +1,63 @@
 require 'ruby2d'
 
-set background: 'navy'
+set background: 'white'
+set fps_cap: 20
 
 GRID_SIZE = 20
+RADIUS = 10
 
 class Snake
 
-    RADIUS = 100
-
-    attr_accessor :direction, :shape, :x, :y
-    def initialize(speed,direction = :right)
-        @x = 50
-        @y = 50
-        @speed = speed
+    attr_accessor :direction
+    def initialize(direction = :right)
+        @start = [[2,2], [3,2], [4,2], [5,2]]
         @direction = direction
     end
     
-    def draw(a = @x, b = @y)
-        @shape = Circle.new(x: @x, y: @y,radius: RADIUS,sectors: 32,color: 'red',z: 11)
-    end
-
-    def movement
-        if @direction == :up
-            @y -= @speed
-        elsif @direction == :down
-            @y += @speed
-        elsif @direction == :left
-            @x -= @speed
-        elsif @direction == :right
-            @x += @speed
+    def draw
+        @start.each do |position|
+            Circle.new(x: position[0] * GRID_SIZE, y: position[1] * GRID_SIZE,radius: RADIUS,sectors: 50,color: 'red',z: 11)
         end
     end
 
-    def increment
-        draw(10,100)
+    def movement
+        @start.shift
+        if @direction == :up
+            @start.push([head[0], head[1] - 1])
+        elsif @direction == :down
+            @start.push([head[0], head[1] + 1])
+        elsif @direction == :right
+            @start.push([head[0] + 1, head[1]])
+        else @direction == :left
+            @start.push([head[0] - 1, head[1]])
+        end     
+    end
+
+    def head
+        @start[-1]
     end
 
     def off_bounds
-        @x <= 0 || @shape.x >= Window.width || @y <= 0 || (@y+RADIUS) >= Window.height
+       # @x <= 0 || @shape.x >= Window.width || @y <= 0 || (@y+RADIUS) >= Window.height
     end
 end
 
 class Bait
 
+    def initialize
+        @bait = [7,3]
+    end
+
     def draw
-        Circle.new(x: 300, y: 100,radius: 3,sectors: 32,color: 'black',z: 10)
+        Circle.new(x: @bait[0] * GRID_SIZE, y: @bait[1] * GRID_SIZE,radius: RADIUS-5,sectors: 32,color: 'brown',z: 10)
+    end
+
+    def ate(snake)
+        snake.head == @bait
     end
 end
 
-snake = Snake.new(1.5)
+snake = Snake.new
 bait = Bait.new
 
 
@@ -66,13 +75,17 @@ end
 
 update do
     clear
+    
+    
     snake.draw
     snake.movement
-    if snake.off_bounds
-        snake = Snake.new(1.5)
-    end
-    snake.increment
+    #if snake.off_bounds
+       # snake = Snake.new(1.5)
+    #end
     bait.draw
+    if bait.ate(snake)
+        puts "smelt it"
+    end
 end
 
 show
