d348506133568f566e1199f01f58d81c7171d97f
Simrin Siby <simrin2002@gmail.com>
2022-10-20 19:39:41 +0530

    modularised game files

---
+++ main.rb
@@ -0,0 +1,34 @@
+require 'ruby2d'
+require './snake'
+
+snake = Snake.new
+bait = Bait.new
+
+
+on :key_down do |event|
+    if event.key == 'w'
+      snake.direction = :up
+    elsif event.key == 's'
+      snake.direction = :down
+    elsif event.key == 'a'
+      snake.direction = :left
+    elsif event.key == 'd'
+      snake.direction = :right
+    end
+end
+
+update do
+    clear
+     
+    snake.draw
+    snake.movement
+    #if snake.off_bounds
+       # snake = Snake.new(1.5)
+    #end
+    bait.draw
+    if bait.ate(snake)
+        puts "smelt it"
+    end
+end
+
+show
