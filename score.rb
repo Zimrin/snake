3e757f37ba216f59e014b7384e64211d1bc2b7c0
Simrin Siby <simrin2002@gmail.com>
2022-10-26 20:12:20 +0530

    score is now counted

---
+++ score.rb
@@ -0,0 +1,21 @@
+class Score
+
+    def initialize()
+      @score = 0
+    end
+  
+    def ate?
+      @score += 1
+    end
+    
+    def draw
+      Text.new(
+        "Score #{@score}",
+        x: 75, y: 25,
+        style: 'bold',
+        size: 20,
+        color: 'blue',
+        z: 11
+      )
+    end
+end
