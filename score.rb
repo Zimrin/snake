24c7bd9d959345d1921728336008849acff409e3
Simrin Siby <simrin2002@gmail.com>
2022-11-15 21:44:46 +0530

    highscore now shows in-game

--- score.rb
+++ score.rb
@@ -3,6 +3,9 @@ class Score
     attr_accessor :score
     def initialize()
       @score = 0
+      file = File.open("highscore.txt")
+      @file_data = file.read
+      file.close
     end
   
     def ate?
@@ -13,6 +16,22 @@ class Score
       Text.new(
         "Score #{@score}",
         x: 75, y: 25,
+        style: 'bold',
+        size: 20,
+        color: 'blue',
+        z: 11
+      )
+      Text.new(
+        "Highscore : ",
+        x: 75, y: 45,
+        style: 'bold',
+        size: 20,
+        color: 'red',
+        z: 11
+      )
+      Text.new(
+        "#{@file_data}",
+        x: 190, y: 45,
         style: 'bold',
         size: 20,
         color: 'blue',
